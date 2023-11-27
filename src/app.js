4f779c9bed580a9baa1ae641f695dfcdc623715b
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-11-28 03:08:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,14 +8,43 @@ const server = http.createServer((req, res) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value1 = obj.num1;
-      const value2 = obj.num2;
+     });
+
+    req.on('end', () => {
+      try {
+        const requestBody = chunks.join('');
+        const obj = JSON.parse(chunks)
+        const value1 = obj.num1;
+        const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      
-    });
-    }
+        if (!Number.isInteger(value1) || value1 <= 0) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('The operation cannot be performed. num1 must be a positive integer.');
+          return;
+        }
+
+        if (!Number.isInteger(value2) || value2 < 0) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Invalid input. num2 must be a non-negative integer.');
+          return;
+        }
+
+        // Calculate the exponentiation
+        const result = Math.pow(value1, value2);
+
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`The result is ${result}`);
+        } 
+        catch (error) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Invalid input. Please provide valid JSON with num1 and num2.');
+        }
+      });
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Endpoint not found.');
+      }
 });
 
 module.exports = server;
